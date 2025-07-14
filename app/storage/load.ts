27ee7c3070a7c56002eb201dd0242e463e356a9f
dorisaward <dorisaward@gmail.com>
2025-07-14 09:04:45 +0100

    Tidy up & tests working

--- app/storage/load.ts
+++ app/storage/load.ts
@@ -13,20 +13,8 @@ async function load(pokemonName: string): Promise<Pokemon | undefined> {
   }
 }
 
-// const results: Promise<Pokemon>[] = [];
-//
-// for (const pokemonName of kantoPokemon) {
-//   const result = load(pokemonName);
-//   if (result) {
-//     results.push(result);
-//   }
-// }
-//
-// return results;
-
 function loadAll(): Promise<Pokemon | undefined>[] {
   return kantoPokemon.map((pokemonName) => load(pokemonName));
 }
 
 export default loadAll;
-// export default load
